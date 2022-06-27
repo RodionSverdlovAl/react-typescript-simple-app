7eced43f33d150da663d9263c0a5eb72fe009725
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-06-27 17:38:52 +0300

    add user interface

---
+++ src/components/UserList.tsx
@@ -0,0 +1,15 @@
+import { IUser } from "../types/types"
+
+interface UserListProps {
+    users: IUser[];
+}
+
+const UserList:React.FC<UserListProps> = ({users}) =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default UserList
